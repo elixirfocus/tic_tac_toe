68f1fdafb5ec1e472f18db0f658a2d9fa650575a
Mike Zornek <zorn@zornlabs.com>
2021-11-20 21:53:33 -0500

    First pass at some basic interface.

--- lib/tic_tac_toe/game.ex
+++ lib/tic_tac_toe/game.ex
@@ -4,48 +4,194 @@ defmodule TicTacToe.Game do
   traditional 3x3 grid game of Tic-Tac-Toe.
   """
 
+  # INFO: I prefer to alias this here so later in the typespec and other pattern matching I can refer to the type / module `Game` instead of `__MODULE__`.
+  alias TicTacToe.Game
+
   @typedoc "The current value of a gird position."
-  @type pos_value :: :empty | :x | :o
+  @type position_value :: :empty | :x | :o
+
+  @type position_name ::
+          :position_0
+          | :position_1
+          | :position_2
+          | :position_3
+          | :position_4
+          | :position_5
+          | :position_6
+          | :position_7
+          | :position_8
 
   @typedoc "The next player allowed to make a turn or game over."
   @type next_turn_value :: :x_player | :o_player | :game_over
 
+  # INFO: For scope/simplicity reasons I will only be storing the current state of the board, though I think it would be interesting to store the full historic move list and render the board from it. Perhaps in a second pass.
+
   @typedoc """
 
   """
-  @type t :: %__MODULE__{
-          grid: %{
-            pos_0: pos_value,
-            pos_1: pos_value,
-            pos_2: pos_value,
-            pos_3: pos_value,
-            pos_4: pos_value,
-            pos_5: pos_value,
-            pos_6: pos_value,
-            pos_7: pos_value,
-            pos_8: pos_value
+  @type t :: %Game{
+          board: %{
+            position_0: position_value,
+            position_1: position_value,
+            position_2: position_value,
+            position_3: position_value,
+            position_4: position_value,
+            position_5: position_value,
+            position_6: position_value,
+            position_7: position_value,
+            position_8: position_value
           },
           next_turn: next_turn_value
         }
 
-  @enforce_keys [:grid, :next_turn]
-  defstruct [:grid, :next_turn]
+  @enforce_keys [:board, :next_turn]
+  defstruct [:board, :next_turn]
 
   @spec new :: TicTacToe.Game.t()
   def new() do
     %__MODULE__{
-      grid: %{
-        pos_0: :empty,
-        pos_1: :empty,
-        pos_2: :empty,
-        pos_3: :empty,
-        pos_4: :empty,
-        pos_5: :empty,
-        pos_6: :empty,
-        pos_7: :empty,
-        pos_8: :empty
+      board: %{
+        position_0: :empty,
+        position_1: :empty,
+        position_2: :empty,
+        position_3: :empty,
+        position_4: :empty,
+        position_5: :empty,
+        position_6: :empty,
+        position_7: :empty,
+        position_8: :empty
       },
       next_turn: :x_player
     }
+  end
+
+  # INFO: We want to provide an explicit interface and not leak the
+  # implementation. In a first pass, we might have historically expected users
+  # of this structure to peek into and find things like `next_turn`, but by
+  # having an interface would could potentially evolve the app to have more than
+  # 2 players or a different sized board.
+
+  @doc """
+  Returns the next player turn of the given game, either
+  `:x_player` or `:o_player`.
+
+  Returns `:game_over` if there are no more player turns.
+
+  ## Examples
+    iex> alias TicTacToe.Game
+    iex> Game.next_turn(Game.new())
+    :x_player
+
+  """
+  @spec next_turn(Game.t()) :: next_turn_value
+  def next_turn(%Game{next_turn: value}), do: value
+
+  # INFO: In the docs I refer to "game" but if this was an external module I
+  # would prefer to refer to `TicTacToe.Game` so as to make it linkable in the
+  # rendered ex_doc output.
+
+  @doc """
+  Returns the position value for the given game and requested position.
+
+  Returns `{:error, :invalid_position}` if the given position is unknown.
+
+  ## Examples
+    iex> alias TicTacToe.Game
+    iex> Game.position_value(Game.new(), :position_3)
+    :empty
+    iex> Game.position_value(Game.new(), :position_foobar)
+    {:error, :invalid_position}
+
+  """
+  @spec position_value(Game.t(), position_name) :: position_value | {:error, :invalid_position}
+  def position_value(%Game{board: board}, position) do
+    Map.get(board, position, {:error, :invalid_position})
+  end
+
+  def play_turn(%Game{} = game, position, value) do
+    with :game_in_progress <- winner?(game),
+         :empty <- position_value(game, position),
+         true <- allowed_turn?(game, value) do
+      game
+      |> update_board(position, value)
+      |> cycle_next_turn()
+    else
+      _ -> {:error, :unplayable_turn}
+    end
+  end
+
+  @spec winner?(Game.t()) :: :game_in_progress | :o_player | :x_player | :tie_game
+  def winner?(game) do
+    cond do
+      check_win(game, :x) == true ->
+        :x_player
+
+      check_win(game, :o) == true ->
+        :o_player
+
+      check_tie(game) == true ->
+        :tie_game
+
+      true ->
+        :game_in_progress
+    end
+  end
+
+  defp check_win(%Game{} = game, value) do
+    has_horizontal_win?(game, value) ||
+      has_vertical_win?(game, value) ||
+      has_cross_win?(game, value)
+  end
+
+  defp check_tie(%Game{board: board}) do
+    :empty not in Map.values(board)
+  end
+
+  defp has_horizontal_win?(%Game{board: board}, value) when value in [:x, :o] do
+    case board do
+      %{position_0: ^value, position_1: ^value, position_2: ^value} -> true
+      %{position_3: ^value, position_4: ^value, position_5: ^value} -> true
+      %{position_6: ^value, position_7: ^value, position_8: ^value} -> true
+      _ -> false
+    end
+  end
+
+  defp has_vertical_win?(%Game{board: board}, value) when value in [:x, :o] do
+    case board do
+      %{position_0: ^value, position_3: ^value, position_6: ^value} -> true
+      %{position_1: ^value, position_4: ^value, position_7: ^value} -> true
+      %{position_2: ^value, position_5: ^value, position_8: ^value} -> true
+      _ -> false
+    end
+  end
+
+  defp has_cross_win?(%Game{board: board}, value) when value in [:x, :o] do
+    case board do
+      %{position_0: ^value, position_4: ^value, position_8: ^value} -> true
+      %{position_2: ^value, position_4: ^value, position_6: ^value} -> true
+      _ -> false
+    end
+  end
+
+  defp allowed_turn?(%Game{next_turn: :x_player}, :x), do: true
+  defp allowed_turn?(%Game{next_turn: :o_player}, :o), do: true
+  defp allowed_turn?(_, _), do: false
+
+  defp update_board(%Game{board: board} = game, position, value) do
+    new_board = put_in(board[position], value)
+    %{game | board: new_board}
+  end
+
+  defp cycle_next_turn(%Game{next_turn: current_turn} = game) do
+    case {winner?(game), current_turn} do
+      {:game_in_progress, :x_player} ->
+        %{game | next_turn: :o_player}
+
+      {:game_in_progress, :o_player} ->
+        %{game | next_turn: :x_player}
+
+      {_, _} ->
+        %{game | next_turn: :game_over}
+    end
   end
 end
---
+++ test/tic_tac_toe/game_test.exs
@@ -0,0 +1,117 @@
+defmodule TicTacToe.GameTest do
+  # INFO: I prefer to always be explicit about async with this `use` call.
+  use ExUnit.Case, async: true
+
+  doctest TicTacToe.Game
+
+  alias TicTacToe.Game
+
+  @valid_positions [
+    :position_0,
+    :position_1,
+    :position_2,
+    :position_3,
+    :position_4,
+    :position_5,
+    :position_6,
+    :position_7,
+    :position_8
+  ]
+
+  describe "new/2" do
+    test "success: creates a game with am empty board and expected next_turn" do
+      assert %Game{} = Game.new()
+    end
+  end
+
+  describe "next_turn/2" do
+    test "success: returns expected first turn for new game" do
+      assert Game.next_turn(Game.new()) == :x_player
+    end
+
+    test "success: returns :game_over when there are no more moves to play" do
+      assert Game.next_turn(tie_game()) == :game_over
+    end
+  end
+
+  describe "position_value/2" do
+    test "success: returns a full empty board for a new game" do
+      game = Game.new()
+
+      for position <- @valid_positions do
+        assert Game.position_value(game, position) == :empty
+      end
+    end
+
+    test "failure: returns expected error if asking for a value at invalid position" do
+      assert Game.position_value(Game.new(), :position_9) == {:error, :invalid_position}
+    end
+  end
+
+  describe "play_turn/2" do
+    test "success: given a new game the X player can record a turn and change the board" do
+      game =
+        Game.new()
+        |> Game.play_turn(:position_4, :x)
+
+      assert Game.next_turn(game) == :o_player
+      assert Game.position_value(game, :position_4) == :x
+    end
+
+    test "failure: given a new game, if the O player attempts to play a turn it is not accepted" do
+      assert {:error, :unplayable_turn} = Game.play_turn(Game.new(), :position_4, :o)
+    end
+
+    test "failure: given a new game, and after the X player has recorded a turn, the O player can not play a turn of the same position" do
+      game =
+        Game.new()
+        |> Game.play_turn(:position_4, :x)
+
+      assert {:error, :unplayable_turn} = Game.play_turn(game, :position_4, :o)
+    end
+  end
+
+  describe "winner/2" do
+    test "success: when given a complete game, will return the winning player" do
+      game =
+        Game.new()
+        |> Game.play_turn(:position_0, :x)
+        |> Game.play_turn(:position_3, :o)
+        |> Game.play_turn(:position_1, :x)
+        |> Game.play_turn(:position_4, :o)
+        |> Game.play_turn(:position_2, :x)
+
+      assert Game.winner?(game) == :x_player
+    end
+
+    test "success: when given an incomplete game, will return `:game_in_progress" do
+      assert Game.winner?(Game.new()) == :game_in_progress
+    end
+
+    test "success: when given a complete game with no winner will return `:tie_game`" do
+      assert Game.winner?(tie_game()) == :tie_game
+    end
+  end
+
+  describe "draw/1" do
+    test "success: when given a game will output a visual representation of it to the console" do
+    end
+  end
+
+  defp tie_game() do
+    # x | o | x
+    # x | o | o
+    # o | x | x
+
+    Game.new()
+    |> Game.play_turn(:position_0, :x)
+    |> Game.play_turn(:position_1, :o)
+    |> Game.play_turn(:position_2, :x)
+    |> Game.play_turn(:position_4, :o)
+    |> Game.play_turn(:position_3, :x)
+    |> Game.play_turn(:position_5, :o)
+    |> Game.play_turn(:position_7, :x)
+    |> Game.play_turn(:position_6, :o)
+    |> Game.play_turn(:position_8, :x)
+  end
+end
